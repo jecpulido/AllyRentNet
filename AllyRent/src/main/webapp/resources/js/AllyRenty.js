999c3dc3ca53443e1e207f008dcf4de1abc05e01
Serger <sergioramirez1097@gmail.com>
2018-09-21 07:54:57 -0500

    JavaMail + GUI Perfil de Usuario

--- AllyRent/src/main/webapp/resources/js/AllyRenty.js
+++ AllyRent/src/main/webapp/resources/js/AllyRenty.js
@@ -2,19 +2,28 @@
 if ((sessionStorage.idusuario === undefined) && (sessionStorage.nombre === undefined)) {
     window.location.href = "http://localhost:8080/AllyRent/index.html";
 } else {
-    $("#iImagen").attr("src",sessionStorage.foto);
+    // $("#iImagen").attr("src",sessionStorage.foto);
     $("#hNombre").text(sessionStorage.nombre);
     $("#sPropietario").text(sessionStorage.rol);
     $("#sCorreo").text(sessionStorage.correo);
     $("#sTelefono").text(sessionStorage.telefono);
     $("#sCiudad").text(sessionStorage.ciudad);
     $("#sPublicaciones").text(sessionStorage.publicaciones);
-    $("#sVehiculos").text(sessionStorage.vehiculos);  
+    $("#sVehiculos").text(sessionStorage.vehiculos);
+
+
+    let nombre = sessionStorage.nombre.split(" ");
+    $("#linkProfile").html("<i class='fa fa-user'></i> " + nombre[0]);
+    $("#nomProfile").html("<i class='fa fa-car'></i> " + sessionStorage.nombre);
+    $("#rolProfile").text(sessionStorage.rol);
+    $("#ciuProfile").html("<strong>Origen: </strong>" + sessionStorage.ciudad);
+    $("#vehProfile").html("<strong>N° de Vehículos: </strong> Este usuario posee " + sessionStorage.vehiculos + " vehiculos registrados.");
+    console.log(sessionStorage.foto);
+    $("#pubProfile").text(sessionStorage.publicaciones);
+    $("#imgProfile").attr("src", "../resources/" + sessionStorage.foto);
+
 
 }
 $(document).ready(function () {
 
 });
-
-
-
