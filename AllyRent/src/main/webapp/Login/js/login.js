b2ce5abeec260dbac80649cfc2776bf784d91a4d
jecpulido <jorgepulido95@hotmail.com>
2018-09-21 15:44:41 -0500

    Login con google
    Se implementa boton para login

--- AllyRent/src/main/webapp/Login/js/login.js
+++ AllyRent/src/main/webapp/Login/js/login.js
@@ -158,3 +158,12 @@ $(document).ready(function () {
     });
 
 });
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log(googleUser);
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  window.location.href = "home.html";
+}
